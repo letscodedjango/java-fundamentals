c7625a28e53324c6777e5a6169179c5bcdb42991
Rajeshwar Sharma <rajeshwar.s@zymr.com>
2019-09-03 15:41:22 +0530

    Added typecasting code

---
+++ src/fundamentals/typecasting/1-ImplicitTypecasting.java
@@ -0,0 +1,33 @@
+// Here we will be dicussing about the one of the type casting Implicit Typecasting
+// Implicit typecasting is nothing but storing the value of lower datatype variable into higher datatype variables
+// In simple type conversion of lower datatype to higher datatype
+// It is also known as WIDENING conversion
+
+class TypeCastImplicit {
+
+public void typecastImplicitly(){
+
+        int x = 20;
+        long y;
+
+        y = x; /// here we are storing smaller datatype variable value to higher datatype variable
+        System.out.println("The value of y after storing x in it : " + y); // 20
+
+        System.out.println("The value of x after storing in y : " + x); // should be 20
+
+        x = 100;
+
+        System.out.println("The value of x after reinitializing : " + x); // should be 100
+
+        System.out.println("The value of y after changing the value of x : " + y); // should be 20
+
+}
+
+public static void main(String args[]){
+
+        TypeCastImplicit tc = new TypeCastImplicit();
+        tc.typecastImplicitly();
+
+}
+
+}
---
+++ src/fundamentals/typecasting/2-ExplicitTypecasting.java
@@ -0,0 +1,35 @@
+// Here we will be dicussing about the one of the type casting Explicit Typecasting
+// Explicit typecasting is nothing but storing the value of higher datatype variable into lower datatype variables
+// In simple type conversion of higher datatype to lower datatype
+// It is also known as NARROWING conversion
+
+class TypeCastExplicit {
+
+public void typecastExplicitly(){
+
+        double x = 567.90;
+        long y;
+
+        //y = x; /// here we are storing higher datatype variable value to lower datatype variable and this will throw error --incompatible types: possible lossy conversion from double to long
+// So to resolve above error we gonna type case explicitly
+        y = (long) x;   // this will convert double to long and then store
+        System.out.println("The value of y after storing x in it : " + y); // 567.90
+
+        System.out.println("The value of x after storing in y : " + x); // should be 567.90
+
+        x = 100.99;
+
+        System.out.println("The value of x after reinitializing : " + x); // should be 100.99
+
+        System.out.println("The value of y after changing the value of x : " + y); // should be 567.90
+
+}
+
+public static void main(String args[]){
+
+        TypeCastExplicit tc = new TypeCastExplicit();
+        tc.typecastExplicitly();
+
+}
+
+}
