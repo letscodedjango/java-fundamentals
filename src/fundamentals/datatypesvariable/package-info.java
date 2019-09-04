3812de1bd4e742f7c9c958c90b7a3fa0ef60c735
Rajeshwar Sharma <rajeshwar.s@zymr.com>
2019-09-04 17:04:44 +0530

    Modified package names

---
+++ src/fundamentals/datatypesvariable/package-info.java
@@ -0,0 +1,10 @@
+/**
+* datatypesvariable package consists class that helps to explain the concept of variables and datatypes in java
+* <p>
+* 1-Datatypes class helps to explain various datatypes on java
+* 2-variables class helps to explaing the various type of variables in java
+* 3-VariablesScope class helps to explain the scoping of different types of variables.
+* </p>
+* @author rsharma
+*/
+package fundamentals.datatypesvariable;
